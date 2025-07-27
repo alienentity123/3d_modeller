1214a36fd8bf200949814ceafc9a3301b74a94aa
zhuzhaob <zhuzhaob@stanford.edu>
2025-07-27 15:12:22 -0700

    another one

--- node.py
+++ node.py
@@ -7,7 +7,6 @@ import numpy
 import sys
 import random
 from collections import defaultdict
-import trackball
 
 class color: 
     MIN_COLOR = 0
@@ -149,3 +148,70 @@ class Interaction(object):
             glutMouseFunc(self.handle_mouse_move)
             glutKeyboardFunc(self.handle_keystroke)
             glutSpecialFunc(self.handle_keystroke)
+
+        def translate(self, x, y, z):
+            """translate the camera """
+            self.translation[0] += x
+            self.translation[1] += y
+            self.translation[2] += z
+
+        def handle_mouse_button(self, button, mode, x, y):
+            """callled when the mouse button is pressed or released"""
+            xSize, ySize = glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT)
+            y = ySize - y #invert the y coordinate b/c OpenGL is inverted 
+            self.mouse_loc = (x, y)
+
+            if mode == GLUT_DOWN:
+                self.pressed = button
+                if button == GLUT_RIGHT_BUTTON:
+                    pass
+                elif button == GLUT_LEFT_BUTTON: # pick using the left side of the mouse 
+                    self.trigger('pick', x, y)
+                elif button == 3: #scroll up
+                    self.translate(0, 0, 1.0)
+                elif button == 4: #scroll down
+                    self.translate(0, 0, -1.0)
+            else: ## mouse press release 
+                self.pressed = None 
+            glutPostRedisplay()
+
+        def handle_mouse_move(self, x, screen_y):
+            """called when the mouse is moved"""
+            xSize, ySize = glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT)
+            y = ySize - screen_y # invert the y coordinate because opengl is inverted 
+
+            if self.pressed is not None:
+                dx = x - self.mouse_loc[0]
+                dy = y - self.mouse_loc[1]
+
+                if self.pressed == GLUT_RIGHT_BUTTON and self.trackball is not None:
+                    #ignore the updated camera location b/c we want to rotate around the origin
+                    self.trackball.drag_to(self.mouse_loc[0], self.mouse_loc[1], dx, dy)
+                elif self.pressed == GLUT_LEFT_BUTTON:
+                    self.trigger('move', x, y)
+                elif self.pressed == GLUT_MIDDLE_BUTTON:
+                    self.translate(dx/60.0, dy/60.0, 0)
+                else:
+                    pass
+                glutPostRedisplay()
+            self.mouse_loc = (x, y)
+        
+
+        def handle_keystroke(self, key, x, screen_y):
+            """called on keyboard input from the user"""
+            xSize, ySize = glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT)
+            y = ySize - screen_y
+            if key =='s':
+                self.trigger('place', 'sphere', x, y)
+            elif key== 'c':
+                self.trigger('place', 'cube', x, y)
+            elif key ==GLUT_KEY_UP:
+                self.trigger('scale', up=True)
+            elif key == GLUT_KEY_DOWN:
+                self.trigger('scale', up=False)
+            elif key == GLUT_KEY_LEFT:
+                self.trigger('rotate_color', forward=True)
+            elif key == GLUT_KEY_RIGHT:
+                self.trigger('rotate_color', forward=False)
+            glutPostRedisplay()
+        
